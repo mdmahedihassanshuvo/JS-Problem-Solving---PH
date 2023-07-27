3e90e1f92238ac7b13c5c7a718c07b7875626dc7
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-07-27 21:16:54 +0600

    roman to int added

---
+++ romToInt.js
@@ -0,0 +1,37 @@
+function romanToInteger(s) {
+    const romanNumerals = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
+  
+    let total = 0;
+    let prevValue = 0;
+  
+    for (let i = s.length - 1; i >= 0; i--) {
+      const numeral = s[i];
+      const value = romanNumerals[numeral];
+      
+      if (value >= prevValue) {
+        total += value;
+      } else {
+        total -= value;
+      }
+      
+      prevValue = value;
+    }
+  
+    return total;
+  }
+  
+  // Test cases
+  console.log(romanToInteger("IX")); 
+  console.log(romanToInteger("XXI")); 
+  console.log(romanToInteger("IV")); 
+  console.log(romanToInteger("XC")); 
+  console.log(romanToInteger("CM"));  
+  
