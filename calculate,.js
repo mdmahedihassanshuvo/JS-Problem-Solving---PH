740e32532f9e63810fc129145f67b24cc6c5c1ad
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-07-27 21:05:31 +0600

    calculateer added

---
+++ calculate,.js
@@ -0,0 +1,30 @@
+function calculator(num1, operator, num2) {
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        if (num2 === 0) {
+          return "Cannot divide by zero!";
+        }
+        return num1 / num2;
+      default:
+        return "Invalid operator";
+    }
+  }
+
+  
+console.log(calculator(5, '+', 3));
+
+console.log(calculator(10, '-', 4));
+
+console.log(calculator(2, '*', 6)); 
+
+console.log(calculator(15, '/', 5)); 
+
+console.log(calculator(10, '/', 0)); 
+
+console.log(calculator(10, '&', 5)); 
